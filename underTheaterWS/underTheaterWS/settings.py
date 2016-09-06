ff750dd1269062358a626b9e7c515497b8434805
javierperini <javierperini90@gmail.com>
2016-09-05 23:20:13 -0300

    subo las configuraciones para mostrar las imagenes en el tamplated de search

--- underTheaterWS/underTheaterWS/settings.py
+++ underTheaterWS/underTheaterWS/settings.py
@@ -75,6 +75,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'underTheaterWS.wsgi.application'
 
 
+
+
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
@@ -127,5 +129,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATIC_ROOT = os.path.join(SETTINGS_DIR, '../local_static/')
--- underTheaterWS/underTheaterWS/urls.py
+++ underTheaterWS/underTheaterWS/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from underTheaterApp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^home/', views.HomeViews.as_view(), name="home"),
     url(r'^search/', views.SearchView.as_view(), name="search"),
 
-]
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
